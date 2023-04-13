43ee468cf5480b0fc05adbfca05f7b106c95a0ec
AleAlle62 <basketallegrini@gmail.com>
2023-04-13 11:27:23 +0200

    Aggiungere i file di progetto.

---
+++ la-mia-pizzeria-static/Controllers/PizzaController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using la_mia_pizzeria_static.Models;
+using Microsoft.Extensions.Hosting;
+
+namespace la_mia_pizzeria_static.Controllers
+{
+    public class PizzaController : Controller
+    {
+		private readonly ILogger<PizzaController> _logger;
+		private readonly PizzaContext _context;
+
+		public PizzaController(ILogger<PizzaController> logger, PizzaContext context)
+		{
+			_logger = logger;
+			_context = context;
+		}
+
+		public IActionResult Index()
+        {
+            var pizzas = _context.Pizzas.ToArray();
+
+            return View(pizzas);
+
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult Detail(int id)
+        {
+            var pizza = _context.Pizzas.SingleOrDefault(p => p.Id == id);
+
+            if (pizza is null)
+            {
+                return NotFound($"Pizza with id {id} not found.");
+            }
+
+            return View(pizza);
+        }
+        public IActionResult Create()
+        {
+            var pizza = new Pizza()
+            {
+                Img = "https://picsum.photos/200/300"
+            };
+
+            return View(pizza);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Pizza pizza)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(pizza);
+            }
+
+			_context.Pizzas.Add(pizza);
+			_context.SaveChanges();
+
+			return RedirectToAction("Index");
+            
+        }
+        
+    }
+}
---
+++ la-mia-pizzeria-static/Models/PizzaContext.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+
+namespace la_mia_pizzeria_static.Models
+{
+    public class PizzaContext : DbContext
+    {
+		public PizzaContext(DbContextOptions<PizzaContext> options) : base(options) { }
+		public DbSet<Pizza> Pizzas { get; set; }
+
+        public void Seed()
+        {
+            if (!Pizzas.Any())
+            {
+                var seed = new Pizza[]
+                {
+            new Pizza
+            {
+                Img = "/img/margherita.jpg",
+                Name = "Margherita",
+                Description = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Mollitia quis est adipisci incidunt rem nostrum ipsam fuga ratione tempora eveniet!",
+                Price = 5
+            },
+            new Pizza
+            {
+                Img =  "/img/brontese.png",
+                Name = "Brontese",
+                Description = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Mollitia quis est adipisci incidunt rem nostrum ipsam fuga ratione tempora eveniet!",
+                Price = 13
+            },
+            new Pizza
+            {
+                Img = "/img/pizza-fritta.jpg",
+                Name = "Pizza fritta",
+                Description = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Mollitia quis est adipisci incidunt rem nostrum ipsam fuga ratione tempora eveniet!",
+                Price = 7
+            }
+                };
+
+                Pizzas.AddRange(seed);
+                SaveChanges();
+            }
+        }
+    }
+}
